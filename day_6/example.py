dd25b17b74a696b3430e030475660805dde7114e
auleki <kodagiwa@gmail.com>
2020-07-13 19:28:35 +0100

    examples for day 6

---
+++ day_6/example.py
@@ -0,0 +1,23 @@
+first_tuple = ()
+second_tuple = tuple()
+
+places = ("bakery", "hostel", "pharmacy")
+
+print(f"places: {places}")
+
+jobs = ("doctors", "lawyers", "engineers", "accountants")
+
+
+job_length = len(jobs)
+
+print(job_length)
+
+first_job = jobs[0]
+
+last_job = jobs[-1]
+
+last_index = len(jobs) - 1
+
+last_job_scnd = jobs[last_index]
+
+print(last_job_scnd)
