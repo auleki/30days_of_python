1543b82e54c1f116d37926c4ada14703361e3cf9
auleki <kodagiwa@gmail.com>
2020-07-14 10:46:14 +0100

    day 7 lessons completed, now the test

--- day_7/example.py
+++ day_7/example.py
@@ -1,6 +1,88 @@
-odd_numbers = {1, 3, 5, 7, 9}
-even_numbers = {2, 4, 6, 8, 10}
+# odd_numbers = {1, 3, 5, 7, 9}
+# even_numbers = {2, 4, 6, 8, 10}
 
-print(len(odd_numbers))
+# # print(len(odd_numbers))
 
-print("Is 10 an odd number? Ans:", 10 in odd_numbers )
+# # print("Is 10 an odd number? Ans:", 10 in odd_numbers )
+
+
+# odd_numbers.update([11, 13, 15, 17])
+# odd_numbers.add(19)
+
+
+# print(odd_numbers)
+
+# val = 9 
+
+# # odd_numbers.remove(val) if val in odd_numbers else print("Value does not exist")
+
+# # odd_numbers.discard(val)
+
+# lst = ['item1', 'item2', 'item3', 'item4', 'item1']
+
+# lst_to_st = set(lst)
+
+# print(lst, lst_to_st)
+
+# odd_numbers = {1, 3, 5, 7, 9}
+# even_numbers = {2, 4, 6, 8, 10}
+
+# numbers = odd_numbers.union(even_numbers)
+
+# numbers = set()
+
+# numbers.update(odd_numbers)
+
+# print(numbers)
+
+
+# odd_numbers = {1, 3, 5, 7, 9}
+# even_numbers = {2, 4, 6, 8, 10}
+
+# gf_hobbies = {"sleeping", "TV", "movies", "praying", "traveling", "shopping"}
+
+# my_hobbies = {"sleeping", "movies", "hobbies", "traveling", "coding", "working"}
+
+# common_hobbies = gf_hobbies.intersection(my_hobbies)
+
+# print(common_hobbies)
+
+# whole_numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+# even_numbers = {0, 2, 4, 6, 8, 10}
+
+# few_numbers = {2, 6}
+
+# # print(few_numbers.issubset(even_numbers))
+
+# print()
+
+# # odd_numbers = even_numbers.difference(whole_numbers)
+# odd_numbers = whole_numbers.difference(even_numbers)
+# # print()
+
+# print(odd_numbers)
+
+
+# whole_numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+# even_numbers = {0, 2, 4, 6, 8, 10}
+
+# few_numbers = {2, 6}
+
+
+# weird_result = whole_numbers.symmetric_difference(even_numbers)
+# # print(even_numbers.symmetric_difference(whole_numbers))
+# print(few_numbers.symmetric_difference(whole_numbers))
+
+
+whole_numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+even_numbers = {0, 2, 4, 6, 8, 10}
+
+few_numbers = {2, 6}
+
+non_existing = {101, 99, 69}
+
+# print(non_existing.disjoint(even_numbers))
+
