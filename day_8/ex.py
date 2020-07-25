83ab509f8d6fceb28c688811fc14ef9e2b689a9c
auleki <kodagiwa@gmail.com>
2020-07-25 22:19:41 +0100

    day 8 shift

---
+++ day_8/ex.py
@@ -0,0 +1,14 @@
+product = {
+    "name": "Jean",
+    "color": "red",
+    "price": 200,
+    "discount": true,
+    "sizes": ["small", "medium", "large"],
+    "seller": {
+        "name":"John Smith", 
+        "phone": 200500606
+    }
+}
+
+product_name = product["name"]
+print(product_name)
